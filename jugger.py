867d347dbed99392fe671b7f3a64eb4b1f96613a
pjaroli31 <33995929+pjaroli31@users.noreply.github.com>
2018-06-25 14:34:23 +0530

    Update jugger.py

--- jugger.py
+++ jugger.py
@@ -1,9 +1,11 @@
 
 import time 
 from googletrans import Translator
-
+count = 0
 while True:
 	text = "This needs to be fixed"
 	trans = Translator()
 	print(trans.translate(text,dest="hi").text)
+	print(count)
+	count += 1
 	time.sleep(15)
